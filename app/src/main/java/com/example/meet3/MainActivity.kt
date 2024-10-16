ad1a9966ecde09fcf1f526e4e5a783ab16ee40cb
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-10-16 16:34:52 +0700

    Kelas yang menyediakan komponen untuk aplikasi Android berbasis Jetpack Compose

---
+++ app/src/main/java/com/example/meet3/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.meet3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.meet3.ui.theme.Meet3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Meet3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose( modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(modifier: Modifier = Modifier){
+
+    Column (horizontalAlignment =  Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()
+    ){
+        Text(
+            text = "Indonesia",
+            style = TextStyle(
+                fontSize =  50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue,
+            ),
+
+        )
+        Text(
+            text = "0",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.logofifa),
+            contentDescription = null)
+
+        Text(
+            text = "Nama : Zahrotul Jannah ",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+        Text(
+            text = "Nim : 20220140066 ",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.gambar),
+            contentDescription = null)  
+
+
+    }
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Meet3Theme {
+        Greeting("Android")
+    }
+}
